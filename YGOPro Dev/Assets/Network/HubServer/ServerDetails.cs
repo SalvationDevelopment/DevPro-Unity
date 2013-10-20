3dfd7e942b40f76eb2c3b956dd09ea92d86a45c2
Damien Lawford <killerdamo@hotmail.co.uk>
2013-10-20 12:46:48 +0100

    Server Address Update
    Client now uses the actual hubserver that all users use

--- YGOPro Dev/Assets/Network/HubServer/ServerDetails.cs
+++ YGOPro Dev/Assets/Network/HubServer/ServerDetails.cs
@@ -6,7 +6,7 @@ namespace DevPro.Network
 	public static class ServerDetails
 	{
 		public static Dictionary<string, ServerInfo> ServerList = new Dictionary<string, ServerInfo>();
-		public static string HubAddress = "86.0.24.143";
+		public static string HubAddress = "91.250.87.52";
 		public static int HubPort = 8933;
 	}
 }
