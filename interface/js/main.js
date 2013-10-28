e4df22eefe266f59afcafd9d82294ee1584ca833
James Gladney <jamezs@angelofcode.com>
2013-10-28 09:04:11 -0400

    Interface initial commit
    Lander page, lobby for a duel. Requestable funtions will be in main.js

---
+++ interface/js/main.js
@@ -0,0 +1,9 @@
+$('.downloadbutton, #lobbycancel').on('click',function(){
+	$('.game').toggle();
+	$('header').toggle();
+});
+
+$('#lobbylock, #majorpopup').on('click',function(){
+	$('#majorpopup').toggle();
+	$
+});
