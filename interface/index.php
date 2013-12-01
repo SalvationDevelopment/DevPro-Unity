3d6469662eefed645f986180c1de2894ac0708f0
James Gladney <jamezs@angelofcode.com>
2013-11-30 21:52:58 -0500

    slight changes

---
+++ interface/index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <title>DevPro Online Version 0.0.1.74</title>
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link href="http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/animation.css">
+
+    <script></script>
+</head>
+
+<body>
+    <div class="rail">
+        <header>
+            <div id="unityPlayer">
+                <div class="missing">
+                    <a href="http://unity3d.com/webplayer/" title="Unity Web Player. Install now!">
+                        <img alt="Unity Web Player. Install now!" src="http://webplayer.unity3d.com/installation/getunity.png" width="193" height="63px" />
+                    </a>
+                </div>
+            </div>
+        </header>
+        <header>
+        <h1>
+            <img src="img/logo.png" alt="" id="logo" />
+            <span style="color:#ff6600; font-family:'MKStencilsansBlack', sans-serif">DevPro</span>Online</h1>
+        <h2>Massively Multiplayer Online Yu-Gi-Oh! Simulator</h2>
+        <ul class="featurelist">
+            <li>Massive</li>
+            <li>Automated</li>
+            <li>Updated</li>
+            <li>Open Source</li>
+
+
+        </ul>
+        <nav class="lander-main">
+            <div class="downloadbutton" style="display:none">Play Now</div>
+            <ul class="hidden">
+                <li>Mirror 1</li>
+                <li>Mirror 2</li>
+                <li>Mirror 3</li>
+                <li>Magnet Link</li>
+            </ul>
+        </nav>
+        <div class="originloading" style="text-align:center; margin-top:">Loading Unity...
+            <br>
+        </div>
+        <div style="font-size:.5em; text-align:center; padding-top:20px;">Version 0 : Production 0 : Revision 0 : Commit 74</div>
+    </header>
+        <div class="controlpanel"></div>
+        <div class="documentbody"></div>
+        <div class="ygopro"></div>
+    </div>
+    <div class="scripts">
+        <script type="text/javascript" src="http://webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/UnityObject2.js" />
+        <script type="application/javascript" src="http://ygopro.de/cardreader/index.php?folder=English&callback=" />
+    </div>
+</body>
+
+</html>
